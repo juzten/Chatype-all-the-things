1b5ecaed20c144dc242f13ecb8a3efdccc7bab20
Justin Spain <juzten@gmail.com>
2014-07-06 20:48:01 -0400

    Took out red text 'cha'
    took out red cha text that shows up after clicking extension

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 // Called when the user clicks on the browser action.
 chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.browserAction.setBadgeText({text: "CHA!"});
+  // chrome.browserAction.setBadgeText({text: "CHA!"});
   chrome.tabs.executeScript(null, {file: "content.js"});
 });
